07b27bacb3580ad1a1333e091c848d72bc3e6e60
Burak Neiş <burakneis@Buraks-MacBook-Pro.local>
2023-03-29 22:07:10 +0300

    add api initializer

---
+++ src/Presentation/Api/ApiInitilazer.cs
@@ -0,0 +1,30 @@
+public static class ApiInitilazer
+{
+    public static void AddApi(this WebApplicationBuilder builder)
+    {
+        builder.Services.AddControllers();
+        builder.Services.AddEndpointsApiExplorer();
+        builder.Services.AddSwaggerGen();
+
+        builder.Services.AddApplication()
+           .AddInfrastructure()
+           .AddEntityFrameworkCore()
+               .AddDbContext(options =>
+               {
+                   options.ConnectionString = builder.Configuration.GetConnectionString("SqlServer") ?? throw new ArgumentNullException("Connection string can not be empty.");
+               });
+    }
+
+    public static void UseApi(this WebApplication app)
+    {
+        if (app.Environment.IsDevelopment())
+        {
+            app.UseSwagger();
+            app.UseSwaggerUI();
+        }
+
+        app.UseHttpsRedirection();
+        app.UseAuthorization();
+        app.MapControllers();
+    }
+}
--- src/Presentation/Api/Program.cs
+++ src/Presentation/Api/Program.cs
@@ -1,29 +1,11 @@
 var builder = WebApplication.CreateBuilder(args);
 {
-    builder.Services.AddControllers();
-    builder.Services.AddEndpointsApiExplorer();
-    builder.Services.AddSwaggerGen();
-
-    builder.Services.AddApplication()
-        .AddInfrastructure()
-        .AddEntityFrameworkCore()
-            .AddDbContext(options =>
-            {
-                options.ConnectionString = builder.Configuration.GetConnectionString("SqlServer") ?? throw new ArgumentNullException("Connection string can not be empty.");
-            });
+    builder.AddApi();
 }
 
 var app = builder.Build();
 {
-    if (app.Environment.IsDevelopment())
-    {
-        app.UseSwagger();
-        app.UseSwaggerUI();
-    }
-
-    app.UseHttpsRedirection();
-    app.UseAuthorization();
-    app.MapControllers();
+    app.UseApi();
 
     app.Run();
 }
